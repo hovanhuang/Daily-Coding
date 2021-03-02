91dc847579233d6e641ecec91fec3de23b61f92e
hovanhuang <seafoodhuang@hotmail.com>
2021-03-01 19:04:52 -0800

    na

--- Daily Coding/src/LCAIII.java
+++ Daily Coding/src/LCAIII.java
@@ -31,5 +31,4 @@ public class LCAIII {
         }
         return null;
     }
-
 }
--- Daily Coding/src/LCAIV.java
+++ Daily Coding/src/LCAIV.java
@@ -1,9 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
 
+// find LCA for nodes
 public class LCAIV {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        Set<TreeNode> nodeSet = new HashSet<>();
+        for(TreeNode node : nodes){
+            nodeSet.add(node);
+        }
+        return helper(root, nodeSet);
+        
+    }
+    private TreeNode helper(TreeNode root, Set<TreeNode> nodeSet){
+        if(root == null){
+            return root;
+        }
+        TreeNode left = helper(root.left, nodeSet);
+        TreeNode right = helper(root.right, nodeSet);
+        if(nodeSet.contains(root) || left != null && right != null){
+            return root;
+        }else if(left == null || right == null){
+            return left == null? right : left;
+        }else{
+            return null;
+        }
+    }
 
 }
