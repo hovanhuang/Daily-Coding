f30944b3faa8b1d5f2771387e4be83eb4bba9bcd
hovanhuang <seafoodhuang@hotmail.com>
2021-03-01 17:33:07 -0800

    today commit

--- Daily Coding/src/LCAII.java
+++ Daily Coding/src/LCAII.java
@@ -5,5 +5,33 @@ public class LCAII {
 		// TODO Auto-generated method stub
 
 	}
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode[] found = new TreeNode[1];
+        found[0] = null;
+        LCAHelper(root, p, q, found);
+        return found[0];
+    }
+    public TreeNode LCAHelper(TreeNode root, TreeNode p, TreeNode q, TreeNode[] found){
+        if(root == null){
+            return root;
+        }
+        TreeNode left  = LCAHelper(root.left, p, q, found);
+        TreeNode right  = LCAHelper(root.right, p, q, found);
+        if(root == q || root == p){
+            if(left != null || right != null){
+                found[0] = root;
+            }  
+            return root;
+        }else{
+            if(left == null && right == null){
+                return null;
+            }else if(left == null || right == null){
+                return left == null ? right : left;
+            }else{
+                found[0] = root;
+            }
+            return root;
+        }
+    }
 
 }
