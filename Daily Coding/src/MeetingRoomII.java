d3523e87500989422211ab3c996adfe8b6d8a8ee
hovanhuang <seafoodhuang@hotmail.com>
2021-03-08 18:35:40 -0800

    3/8/2021

--- Daily Coding/src/MeetingRoomII.java
+++ Daily Coding/src/MeetingRoomII.java
@@ -1,4 +1,22 @@
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
 public class MeetingRoomII {
-
+	public int minMeetingRooms(int[][] intervals) {
+		if(intervals == null || intervals.length == 0){
+			return 0;
+		}
+		Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+		pq.offer(intervals[0]);
+		int meetR = 1;
+		for(int i = 1; i < intervals.length; i++){
+			if(intervals[i][0] >= pq.peek()[1]){
+				pq.poll();
+			}
+			pq.offer(intervals[i]);
+			meetR = Math.max(meetR, pq.size());
+		}
+		return meetR;
+	}
 }
