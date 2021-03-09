85c817cec8cb7967f94c797d654d898acf415bb0
hovanhuang <seafoodhuang@hotmail.com>
2021-03-08 18:41:07 -0800

    n/a

--- Daily Coding/src/MeetingRooms.java
+++ Daily Coding/src/MeetingRooms.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class MeetingRooms {
 
@@ -5,5 +6,17 @@ public class MeetingRooms {
 		// TODO Auto-generated method stub
 
 	}
+    public boolean canAttendMeetings(int[][] intervals) {
+        if(intervals == null || intervals.length == 0){
+            return true;
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int prevEnd = Integer.MIN_VALUE;
+        for(int[] arr : intervals){
+            if(prevEnd > arr[0]) return false;
+            prevEnd = arr[1];
+        }
+        return true;
+    }
 
 }
